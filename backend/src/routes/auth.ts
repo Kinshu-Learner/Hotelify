b0d071a9479efe407d19a23087f9610830b5d148
Kinshu-Learner <kinshurajgupta1@gmail.com>
2023-12-29 23:32:53 +0530

    Login functionality

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { check, validationResult } from 'express-validator';
 import User from '../models/user';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -29,7 +30,21 @@ router.use("/login", [
             return res.status(400).json({ message: "Invalid Credentials" });   
         }
 
-        
+        const token = jwt.sign(
+            { userId: user.id },
+            process.env.JWT_SECRET_KEY as string,
+            {
+                expiresIn: "1d"
+            }
+        );
+
+        res.cookie("auth_token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",      // Cuz we only want it on HTTPS in production build, but NOT in development build
+            maxAge: 86400000,
+        });
+
+        res.status(200).json({userId: user._id});
 
     } catch (error) {
         console.log(error);
