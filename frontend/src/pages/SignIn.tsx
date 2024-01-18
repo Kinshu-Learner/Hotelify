41eb2f428bb1ec6541f2007e9e7286a7628a0641
Kinshu-Learner <kinshurajgupta1@gmail.com>
2024-01-19 02:06:58 +0530

    Sign in basic page complete

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,4 +1,6 @@
 import { useForm } from "react-hook-form";
+import { useMutation } from "react-query";
+import * as apiClient from "../api-client";
 
 export type SignInFormData = {
   email: string;
@@ -9,9 +11,29 @@ const SignIn = () => {
   const {
     register,
     formState: { errors },
+    handleSubmit,
   } = useForm<SignInFormData>();
+
+  const mutation = useMutation(apiClient.signIn, {
+    onSuccess: async () => {
+      console.log("User has been signed in");
+      // 1. show the toast
+      // 2. Navigate to the home page
+    },
+    onError: (error: Error) => {
+      // Show the toast
+    },
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    mutation.mutate(data);
+  });
+
   return (
-    <form className="flex bg-violet-100 p-5 rounded-lg flex-col gap-5">
+    <form
+      className="flex bg-violet-100 p-5 rounded-lg flex-col gap-5"
+      onSubmit={onSubmit}
+    >
       <h2 className="text-3xl font-bold">Sign In</h2>
 
       <label className="text-gray-700 text-sm font-bold flex-1">
@@ -43,6 +65,15 @@ const SignIn = () => {
           <span className="text-yellow-600">{errors.password.message}</span>
         )}
       </label>
+
+      <span>
+        <button
+          type="submit"
+          className="bg-indigo-600 text-yellow-300 text-xl rounded-md py-2 px-4 font-bold hover:bg-indigo-500 hover:text-white duration-200"
+        >
+          Login
+        </button>
+      </span>
     </form>
   );
 };
