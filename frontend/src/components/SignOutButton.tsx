a6bd4c76cc96462683022550418d1d6455e68222
Kinshu-Learner <kinshurajgupta1@gmail.com>
2024-01-22 03:35:06 +0530

    Sign Out functionality (start)

--- frontend/src/components/SignOutButton.tsx
+++ frontend/src/components/SignOutButton.tsx
@@ -1,6 +1,25 @@
+import { useMutation } from "react-query";
+import * as apiClient from "../api-client";
+
 const SignOutBUtton = () => {
+  const mutation = useMutation(apiClient.signOut, {
+    onSuccess: () => {
+      // Show toast
+    },
+    onError: () => {
+      // show toast
+    },
+  });
+
+  const handleClick = () => {
+    mutation.mutate();
+  };
+
   return (
-    <button className="flex items-center rounded-sm bg-yellow-300 text-indigo-700 px-3 font-bold hover:bg-yellow-200 duration-100">
+    <button
+      onClick={handleClick}
+      className="flex items-center rounded-sm bg-yellow-300 text-indigo-700 px-3 font-bold hover:bg-yellow-200 duration-100"
+    >
       Sign Out
     </button>
   );
