268f20c6d240594c0ac2cdbed205121f3023fdfc
Kinshu-Learner <kinshurajgupta1@gmail.com>
2024-01-25 02:58:04 +0530

    Set up sign in test

---
+++ e2e-tests/tests/auth.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from "@playwright/test";
+
+const UI_URL = "http://localhost:5173/";
+
+test("should allow the user to sign in", async ({ page }) => {
+  await page.goto(UI_URL);
+
+  // get the sign in button
+  await page.getByRole("link", { name: "Sign In" }).click();
+
+  // check whether we've reached the sign in page by checking the heading
+  await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
+
+  // Filling the sign in form
+  await page.locator("[name=email]").fill("1@1.com");
+  await page.locator("[name=password]").fill("password");
+
+  // Clicking the login button
+  await page.getByRole("button", { name: "Login" }).click();
+
+  // checking that the toast appears
+  await expect(page.getByText("Signed in Successfully!")).toBeVisible();
+
+  // checking if the header links changed after logging in
+  await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
+});
