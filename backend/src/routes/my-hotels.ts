fe26288ad5b67bb271f82c4f732b76ac45861538
Kinshu-Learner <kinshurajgupta1@gmail.com>
2024-01-30 19:03:49 +0530

    Cloudinary image upload

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import multer from "multer"; // multer is a package that handles images/forms
+import cloudinary from "cloudinary";
 
 const router = express.Router();
 
@@ -20,6 +21,21 @@ router.post(
     try {
       const imageFiles = req.files as Express.Multer.File[]; // to get the image data separately
       const newHotel = req.body; // to get the rest of the form data
+
+      // 1. Upload the images to cloudinary
+      const uploadPromises = imageFiles.map(async (image) => {
+        // Looping over all of the images we got in input in "imageFiles".
+        const b64 = Buffer.from(image.buffer).toString("base64"); // Creating a base 64 buffer from each image.
+        let dataURI = "data:" + image.mimetype + ";base64," + b64; // Creating a string to identify the image separately from one another while uploading. mimetype = jpeg/png etc.
+        const res = await cloudinary.v2.uploader.upload(dataURI); // Uploading the image to cloudinary using the string that we just created.
+        return res.url; // Finally returning the url of the uploaded image.
+      });
+
+      const imageURLs = await Promise.all(uploadPromises); // The "uploadPromises" will contain promises of all the images. We want to first await all of them first, and then move on with the next steps.
+
+      // 2. if the upload was successful, add the URLs to the new hotel
+      // 3. save the new hotel in database
+      // 4. return 201 status
     } catch (error) {}
   }
 );
