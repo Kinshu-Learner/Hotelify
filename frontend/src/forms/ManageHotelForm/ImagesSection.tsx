24d3147f3284c1602ea9b6243e5554a9931a5408
Kinshu-Learner <kinshurajgupta1@gmail.com>
2024-02-03 02:04:00 +0530

    Added Images section to manage hotel form

---
+++ frontend/src/forms/ManageHotelForm/ImagesSection.tsx
@@ -0,0 +1,46 @@
+import { useFormContext } from "react-hook-form";
+import { HotelFormData } from "./ManageHotelForm";
+
+const ImagesSection = () => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<HotelFormData>();
+
+  return (
+    <div className="">
+      <h2 className="text-2xl font-bold mb-3">Images</h2>
+
+      <div className="border rounded p-4 flex flex-col gap-4">
+        <input
+          type="file"
+          multiple // So that user can add multiple files
+          accept="image/*" // Only of the type image
+          className="w-full text-gray-700 font-normal"
+          {...register("imageFiles", {
+            validate: (imageFiles) => {
+              const totalLength = imageFiles.length;
+
+              if (totalLength === 0) {
+                return "At least one image should be added";
+              }
+
+              if (totalLength > 6) {
+                return "Total number of images cannot be more than 6";
+              }
+
+              return true;
+            },
+          })}
+        />
+      </div>
+      {errors.imageFiles && (
+        <span className="text-yellow-600 text-sm font-bold">
+          {errors.imageFiles.message}
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default ImagesSection;
--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -3,6 +3,7 @@ import DetailsSection from "./DetailsSection";
 import TypeSection from "./TypeSection";
 import FacilitiesSection from "./FacilitiesSection";
 import GuestsSection from "./GuestsSection";
+import ImagesSection from "./ImagesSection";
 
 export type HotelFormData = {
   name: string;
@@ -20,15 +21,32 @@ export type HotelFormData = {
 
 const ManageHotelForm = () => {
   const formMethods = useForm<HotelFormData>(); // Instead of destructuring register etc fns from useForm, we're taking all of those properties and assigning then to a single variable (formMethods).
+  const { handleSubmit } = formMethods;
+
+  const onSubmit = handleSubmit((formData: HotelFormData) => {
+    console.log(formData);
+  });
   return (
     // Spreading all the properties (in formMethods) as children to the FormProvider.
     // We're technically defining a formContext here.
     <FormProvider {...formMethods}>
-      <form className="flex bg-violet-100 p-5 rounded-lg flex-col gap-10">
+      <form
+        className="flex bg-violet-100 p-5 rounded-lg flex-col gap-10"
+        onSubmit={onSubmit}
+      >
         <DetailsSection />
         <TypeSection />
         <FacilitiesSection />
         <GuestsSection />
+        <ImagesSection />
+        <span className="flex justify-end">
+          <button
+            type="submit"
+            className="bg-indigo-600 text-white text-xl rounded py-2 px-6 font-bold hover:bg-indigo-500 hover:text-yellow-300 duration-200"
+          >
+            Save
+          </button>
+        </span>
       </form>
     </FormProvider>
   );
