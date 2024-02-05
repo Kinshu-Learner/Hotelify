224ffc6e8eb015cb865a0fb6708d705fd602621f
Kinshu-Learner <kinshurajgupta1@gmail.com>
2024-02-06 02:32:22 +0530

    my-hotels section basic card

--- frontend/src/pages/MyHotels.tsx
+++ frontend/src/pages/MyHotels.tsx
@@ -10,6 +10,12 @@ const MyHotels = () => {
       onError: () => {},
     }
   );
+
+  if (!hotelData) {
+    // so that hotelData doesn't go "undefined" in such cases, then we'll have an error while displaying it normally.
+    return <span>No Hotels Found</span>;
+  }
+
   return (
     <div className="space-y-5">
       <span className="flex justify-between">
@@ -21,6 +27,19 @@ const MyHotels = () => {
           Add Hotel
         </Link>
       </span>
+      <div className="flex flex-col gap-8">
+        {hotelData.map((hotel) => (
+          <div className="flex flex-col justify-between bg-violet-100 rounded-lg p-8 gap-5">
+            <h2 className="text-2xl font-bold">{hotel.name}</h2>
+            <div className="whitespace-pre-line">{hotel.description}</div>
+            <div className="grid grid-cols-5 gap-2">
+              <div className="border bg-violet-200 border-slate-300 rounded p-3 flex items-center">
+                {hotel.city}, {hotel.country}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
