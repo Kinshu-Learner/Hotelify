9e51b2d279a04b5ad6fa11f71957629e7bf30a35
Kinshu-Learner <kinshurajgupta1@gmail.com>
2024-02-20 02:52:02 +0530

    added the handleDelete button and totalLength check

--- frontend/src/forms/ManageHotelForm/ImagesSection.tsx
+++ frontend/src/forms/ManageHotelForm/ImagesSection.tsx
@@ -32,7 +32,10 @@ const ImagesSection = () => {
             {existingImageUrls.map((url) => (
               <div className="relative group">
                 <img src={url} className="min-h-full object-cover rounded" />
-                <button className="absolute inset-0 flex items-center justify-center rounded bg-indigo-900 bg-opacity-50 opacity-0 group-hover:opacity-100 text-white duration-200 font-semibold">
+                <button
+                  onClick={(event) => handleDelete(event, url)}
+                  className="absolute inset-0 flex items-center justify-center rounded bg-indigo-900 bg-opacity-50 opacity-0 group-hover:opacity-100 text-white duration-200 font-semibold"
+                >
                   Delete
                 </button>
               </div>
@@ -46,7 +49,8 @@ const ImagesSection = () => {
           className="w-full text-gray-700 font-normal"
           {...register("imageFiles", {
             validate: (imageFiles) => {
-              const totalLength = imageFiles.length;
+              const totalLength =
+                imageFiles.length + (existingImageUrls?.length || 0); // This is so that if user is editing hotel, and they've already uploaded some images, he total count still shouldn't go more than 6.
 
               if (totalLength === 0) {
                 return "At least one image should be added";
