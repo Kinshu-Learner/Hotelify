e1dcefbe2f51ec817d03a105a99abca821c91613
Kinshu-Learner <kinshurajgupta1@gmail.com>
2024-02-26 00:54:30 +0530

    added the constructSearchQuery to search hotels

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -7,6 +7,8 @@ const router = express.Router();
 // /api/hotels/search?
 router.get("/search", async (req: Request, res: Response) => {
   try {
+    const query = constructSearchQuery(req.query);
+
     const pageSize = 5;
     const pageNumber = parseInt(
       req.query.page ? req.query.page.toString() : "1"
@@ -14,9 +16,9 @@ router.get("/search", async (req: Request, res: Response) => {
 
     const skip = (pageNumber - 1) * pageSize;
 
-    const hotels = await Hotel.find().skip(skip).limit(pageSize);
+    const hotels = await Hotel.find(query).skip(skip).limit(pageSize);
 
-    const total = await Hotel.countDocuments();
+    const total = await Hotel.countDocuments(query);
 
     const response: HotelSearchResponse = {
       data: hotels,
@@ -33,5 +35,31 @@ router.get("/search", async (req: Request, res: Response) => {
     res.status(500).json({ message: "Something went wrong" });
   }
 });
+
+const constructSearchQuery = (queryParams: any) => {
+  let constructedQuery: any = {};
+
+  if (queryParams.destination) {
+    constructedQuery.$or = [
+      // in MongoDB, $or performs logical OR bw two or more query expressions in the array
+      { city: new RegExp(queryParams.destination, "i") }, // "i" specifies case-insensitive match
+      { country: new RegExp(queryParams.destination, "i") },
+    ];
+  }
+
+  if (queryParams.adultCount) {
+    constructedQuery.adultCount = {
+      $gte: parseInt(queryParams.adultCount), // MongoDB, "$gte": greater than or equal to
+    };
+  }
+
+  if (queryParams.childCount) {
+    constructedQuery.childCount = {
+      $gte: parseInt(queryParams.childCount),
+    };
+  }
+
+  return constructedQuery;
+};
 
 export default router;
