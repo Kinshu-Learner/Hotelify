d8d8de9b7100cb176c060af2310baf52f68f89d2
Kinshu-Learner <kinshurajgupta1@gmail.com>
2024-04-12 17:59:04 +0530

    added checkin, checkout dates on bookingdetailsummary

---
+++ frontend/src/components/BookingDetailSummary.tsx
@@ -0,0 +1,47 @@
+import { HotelType } from "../shared/types";
+
+type Props = {
+    checkIn: Date;
+    checkOut: Date;
+    adultCount: number;
+    childCount: number;
+    numberOfNights: number;
+    hotel: HotelType;
+}
+
+const BookingDetailSummary = ({
+    checkIn,
+    checkOut,
+    adultCount,
+    childCount,
+    numberOfNights,
+    hotel
+}: Props) => {
+
+    return (
+        <div className="grid gap-4 rounded-lg border border-slate-300 p-5 h-fit">
+            <h2 className="text-xl font-bold">
+                Your Booking Details
+            </h2>
+            <div className="border-b py-2">
+                Location:
+                <div className="font-bold">
+                    {`${hotel.name}, ${hotel.city}, ${hotel.country}`}
+                </div>
+            </div>
+
+            <div className="flex justify-between">
+                <div className="">
+                    Check-in
+                    <div className="font-bold">{checkIn.toDateString()}</div>
+                </div>
+                <div className="">
+                    Check-out
+                    <div className="font-bold">{checkOut.toDateString()}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BookingDetailSummary;
