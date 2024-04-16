379f54e181921301ce2df114d1764758950f62cb
Kinshu-Learner <kinshurajgupta1@gmail.com>
2024-04-17 02:47:59 +0530

    created bookingType, bookingSchema, and stripe booking endpoint

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 import Hotel from "../models/hotel";
-import { HotelSearchResponse } from "../shared/types";
+import { BookingType, HotelSearchResponse } from "../shared/types";
 import { param, validationResult } from "express-validator";
 import Stripe from "stripe";
 import verifyToken from "../middleware/auth";
@@ -57,62 +57,6 @@ router.get("/search", async (req: Request, res: Response) => {
     res.status(500).json({ message: "Something went wrong" });
   }
 });
-
-const constructSearchQuery = (queryParams: any) => {
-  let constructedQuery: any = {};
-
-  if (queryParams.destination) {
-    constructedQuery.$or = [
-      // in MongoDB, $or performs logical OR bw two or more query expressions in the array
-      { city: new RegExp(queryParams.destination, "i") }, // "i" specifies case-insensitive match
-      { country: new RegExp(queryParams.destination, "i") },
-    ];
-  }
-
-  if (queryParams.adultCount) {
-    constructedQuery.adultCount = {
-      $gte: parseInt(queryParams.adultCount), // MongoDB, "$gte": greater than or equal to
-    };
-  }
-
-  if (queryParams.childCount) {
-    constructedQuery.childCount = {
-      $gte: parseInt(queryParams.childCount),
-    };
-  }
-
-  if (queryParams.facilities) {
-    constructedQuery.facilities = {
-      $all: Array.isArray(queryParams.facilities) // Matches results that matches "all" facilities.
-        ? queryParams.facilities // ensures that the value assigned to $all is always an array. If queryParams.facilities is already an array, it uses it as is. Otherwise, it creates a new array containing "queryParams.facilities". This is necessary because sometimes users might provide a single facility as a string instead of an array.
-        : [queryParams.facilities],
-    };
-  }
-
-  if (queryParams.types) {
-    constructedQuery.type = {
-      $in: Array.isArray(queryParams.types) // "$in" cuz a hotel can only have 1 type, but the user can enter multiple in search.
-        ? queryParams.types
-        : [queryParams.types],
-    };
-  }
-
-  if (queryParams.stars) {
-    const starRatings = Array.isArray(queryParams.stars)
-      ? queryParams.stars.map((star: string) => parseInt(star)) // Each star rating will be a string, converting all to int.
-      : parseInt(queryParams.stars); // If only one star rating is present, converting it to int.
-
-    constructedQuery.starRating = { $in: starRatings };
-  }
-
-  if (queryParams.maxPrice) {
-    constructedQuery.pricePerNight = {
-      $lte: parseInt(queryParams.maxPrice).toString(),
-    };
-  }
-
-  return constructedQuery;
-};
 
 router.get(
   "/:id",
@@ -175,5 +119,115 @@ router.post(
     res.send(response);
   }
 );
+
+router.post(
+  "/:hotelId/bookings",
+  verifyToken,
+  async (req: Request, res: Response) => {
+    try {
+      const paymentIntentId = req.body.paymentIntentId;
+
+      const paymentIntent = await stripe.paymentIntents.retrieve(
+        paymentIntentId as string
+      );
+
+      if (!paymentIntent) {
+        return res.status(400).json({ message: "Payment intent not found" });
+      }
+
+      if (
+        paymentIntent.metadata.hotelId !== req.params.hotelId ||
+        paymentIntent.metadata.userId !== req.userId
+      ) {
+        return res.status(400).json({ message: "Payment intent mismatched" });
+      }
+
+      if (paymentIntent.status !== "succeeded") {
+        return res.status(400).json({
+          message: `Payment intent not succeeded. Status: ${paymentIntent.status}`,
+        });
+      }
+
+      const newBooking: BookingType = {
+        ...req.body,
+        userId: req.userId,
+      };
+
+      const hotel = await Hotel.findOneAndUpdate(
+        { _id: req.params.hotelId },
+        {
+          $push: { bookings: newBooking },
+        }
+      );
+
+      if (!hotel) {
+        return res.status(404).json({ message: "Hotel not found" });
+      }
+
+      await hotel.save();
+
+      res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+);
+
+const constructSearchQuery = (queryParams: any) => {
+  let constructedQuery: any = {};
+
+  if (queryParams.destination) {
+    constructedQuery.$or = [
+      // in MongoDB, $or performs logical OR bw two or more query expressions in the array
+      { city: new RegExp(queryParams.destination, "i") }, // "i" specifies case-insensitive match
+      { country: new RegExp(queryParams.destination, "i") },
+    ];
+  }
+
+  if (queryParams.adultCount) {
+    constructedQuery.adultCount = {
+      $gte: parseInt(queryParams.adultCount), // MongoDB, "$gte": greater than or equal to
+    };
+  }
+
+  if (queryParams.childCount) {
+    constructedQuery.childCount = {
+      $gte: parseInt(queryParams.childCount),
+    };
+  }
+
+  if (queryParams.facilities) {
+    constructedQuery.facilities = {
+      $all: Array.isArray(queryParams.facilities) // Matches results that matches "all" facilities.
+        ? queryParams.facilities // ensures that the value assigned to $all is always an array. If queryParams.facilities is already an array, it uses it as is. Otherwise, it creates a new array containing "queryParams.facilities". This is necessary because sometimes users might provide a single facility as a string instead of an array.
+        : [queryParams.facilities],
+    };
+  }
+
+  if (queryParams.types) {
+    constructedQuery.type = {
+      $in: Array.isArray(queryParams.types) // "$in" cuz a hotel can only have 1 type, but the user can enter multiple in search.
+        ? queryParams.types
+        : [queryParams.types],
+    };
+  }
+
+  if (queryParams.stars) {
+    const starRatings = Array.isArray(queryParams.stars)
+      ? queryParams.stars.map((star: string) => parseInt(star)) // Each star rating will be a string, converting all to int.
+      : parseInt(queryParams.stars); // If only one star rating is present, converting it to int.
+
+    constructedQuery.starRating = { $in: starRatings };
+  }
+
+  if (queryParams.maxPrice) {
+    constructedQuery.pricePerNight = {
+      $lte: parseInt(queryParams.maxPrice).toString(),
+    };
+  }
+
+  return constructedQuery;
+};
 
 export default router;
