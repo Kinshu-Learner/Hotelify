f16ee5855c7658ddc8e2457cbf8fdbb5cc9d98c0
Kinshu-Learner <kinshurajgupta1@gmail.com>
2024-05-04 02:06:25 +0530

    wrote the book hotel test (minor issue)

--- e2e-tests/tests/search-hotels.spec.ts
+++ e2e-tests/tests/search-hotels.spec.ts
@@ -44,3 +44,70 @@ test("should show hotel detail", async ({ page }) => {
   await expect(page).toHaveURL(/detail/);
   await expect(page.getByRole("button", { name: "Book Now" })).toBeVisible();
 });
+
+test("should book hotel", async ({ page }) => {
+  // Everything from the should show hotel detail test, and more
+  await page.goto(UI_URL);
+
+  await page.getByPlaceholder("Where are you going?").fill("Test City");
+
+  const date = new Date();
+  date.setDate(date.getDate() + 3);
+  const formattedDate = date.toISOString().split("T")[0];
+
+  await page.getByPlaceholder("Check-out Date").fill(formattedDate);
+
+  await page.getByRole("button", { name: "Search" }).click();
+
+  await page.getByText("Test Hotel").first().click();
+
+  await page.getByRole("button", { name: "Book Now" }).click();
+
+  await expect(page.getByText("Total Cost: Rs. 200.00")).toBeVisible();
+
+  const stripeFrame = page.frameLocator("iframe").first();
+  await stripeFrame
+    .locator('[placeholder="Card number"]')
+    .fill("4000003560000008");
+  await stripeFrame.locator('[placeholder="MM / YY"]').fill("04/30");
+  await stripeFrame.locator('[placeholder="CVC"]').fill("123");
+
+  await page.getByRole("button", { name: "Confirm Booking" }).click();
+
+  // TODO: not able to click on the "COMPLETE" button
+
+  // await page.getByRole("button", { name: "Complete" }).click();
+  // await expect(page.getByText("Booking Saved!")).toBeVisible();
+
+  // Wait for the popup to appear
+  // const popup = await page.waitForEvent("popup");
+
+  // // Get the browser context of the popup
+  // const popupContext = await popup.context();
+
+  // // Create a new page in the popup context
+  // const popupPage = await popupContext.newPage();
+
+  // // Click the "Complete" button on the popup
+  // await popupPage.getByRole("button", { name: "COMPLETE" }).click();
+
+  // // Close the popup page
+  // await popupPage.close();
+
+  // // Close the popup context
+  // await popupContext.close();
+
+  // await expect(page.getByText("Booking Saved!")).toBeVisible();
+
+  await page.getByRole("button", { name: "Confirm Booking" }).click();
+
+  // Wait for the Stripe 3D Secure 2 Test Page iframe to be available
+  const stripeFrame2 = await await page
+    .frameLocator('iframe[name^="stripe_3ds2_or_"]')
+    .first();
+
+  // Click the "Complete" button on the Stripe 3D Secure 2 Test Page
+  await stripeFrame2.getByRole("button", { name: "COMPLETE" }).click();
+
+  await expect(page.getByText("Booking Saved!")).toBeVisible();
+});
